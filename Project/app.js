553fc083649cdcf9702f86929c7e24204b1aa03c
Cillian23 <cotoole7@tcd.ie>
2025-04-28 15:27:28 +0300

    improved menu and commented code

--- Project/app.js
+++ Project/app.js
@@ -8,36 +8,43 @@ const profcanc = document.querySelector('button[name="cancel"]');
 const viewThesis = document.querySelector('button.men.thes');
 const thesStatus = document.querySelectorAll('.thesis');
 const popVisi = document.querySelector('.popup');
+const popCanc = document.querySelector('button.popbtn');
 let theStatus=0;
 
-//Add event listeners for student page, these don't work properly yet, messed up when click one of the other buttons to close a menu
+//Add event listeners for student page, only work if popup menu is currently inactive
 
-viewTopic.addEventListener('click', () => {
-    
+popCanc.addEventListener('click', () => {     //Adding X out button for each menu, which turns off all menus, stops buttons interferinf with each other 
+    popVisi.classList.toggle('inactive');
+    popVisi.classList.toggle('active');
+    studentTopic.classList.remove('active');
+    profile.classList.remove('active');
+    thesStatus[theStatus].classList.remove('active');
+    studentTopic.classList.add('inactive');
+    profile.classList.add('inactive');
+    thesStatus[theStatus].classList.add('inactive');
+})
+viewTopic.addEventListener('click', () => {      //Button for viewing thesis topic
+    if(popVisi.classList.contains('inactive')){
+
         studentTopic.classList.toggle('active');
-        popVisi.classList.toggle('active');
         studentTopic.classList.toggle('inactive');
         popVisi.classList.toggle('inactive');
+        popVisi.classList.toggle('active');
+    }       
 });
 
-editPro.addEventListener('click', () => {
+editPro.addEventListener('click', () => {     // Button for editing profile details
     console.log("clicked");
-    /*if(profile.classList.contains('inactive')){
-        profile.classList.remove('inactive');
-        profile.classList.add('active');
-    }*/
+    if(popVisi.classList.contains('inactive')){
+
         profile.classList.toggle('active');
-        popVisi.classList.toggle('active');
         profile.classList.toggle('inactive');
         popVisi.classList.toggle('inactive');
-
+        popVisi.classList.toggle('active');
+    }
 });
 
-profcanc.addEventListener('click', () => {
-   /* if(profile.classList.contains('active')){
-        profile.classList.remove('active');
-        profile.classList.add('inactive');
-    }*/
+profcanc.addEventListener('click', () => {   // Button for cancelling edit profile
         profile.classList.toggle('active');
         popVisi.classList.toggle('active');
         profile.classList.toggle('inactive');
@@ -45,13 +52,14 @@ profcanc.addEventListener('click', () => {
 })
 
 
-viewThesis.addEventListener('click', () => {
-    
+viewThesis.addEventListener('click', () => {  //Button for viewing thesis status, array as depends on current thesis status, arbitrary variable at the moment
+    if(popVisi.classList.contains('inactive')){
+
         thesStatus[theStatus].classList.toggle('active');
-        popVisi.classList.toggle('active');
         thesStatus[theStatus].classList.toggle('inactive');
         popVisi.classList.toggle('inactive');
-    
+        popVisi.classList.toggle('active');
+    }  
 });
 
 
